dfed40d16a6eaa36b27b95e853bab26f445fe29c
Richardh Harper <dick.harper@gmail.com>
2022-12-20 06:52:08 -0700

    Initial commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './security/login/login.component';
+import { ResetPasswordComponent } from './security/reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'reset', component: ResetPasswordComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [
+  HomeComponent,
+  LoginComponent,
+  PageNotFoundComponent,
+  ResetPasswordComponent,
+];
